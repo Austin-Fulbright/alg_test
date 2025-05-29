75b8e531b4420b088c0fcee5fdd67b12346370b1
Austin-Fulbright <austinfulbright@gmail.com>
2025-05-29 14:54:38 -0400

    added solutions for second lc derivative

--- maximize-target-nodes/main_1.py
+++ maximize-target-nodes/main_1.py
@@ -1,21 +1,46 @@
+import sys
 import json
-from solution_1 import evenSubsetMax
 
-def run_tests():
-    with open('test_cases_1.json', 'r') as f:
-        test_cases = json.load(f)
+from pathlib import Path
+from solution_1 import Solution
 
-    for idx, case in enumerate(test_cases, 1):
-        nums = case['input']
-        expected = case['expected']
-        result = evenSubsetMax(nums)
-        status = "PASS" if result == expected else "FAIL"
-        # Option #1: convert to string and pad
-        print(f"Test {idx:>2}: input={str(nums):<15} expected={expected:<3} got={result:<3} → {status}")
+def load_test_cases(filename):
 
-        # — OR —
-        # Option #2: use repr without padding
-        # print(f"Test {idx:>2}: input={nums!r} expected={expected:<3} got={result:<3} → {status}")
+    current_dir = Path(__file__).parent
+    test_cases_path = current_dir / "test_cases_1.json"
+
+    try:
+        with open (test_cases_path, 'r') as f:
+            data = json.load(f)
+        return data.get("test_case", [])
+    except Exception as e:
+        print(f"Error reading {filename}: {e}")
+        sys.exit(1)
+
+def main():
+    sol = Solution()
+
+    test_cases = load_test_cases("test_cases_1.json")
+    for i, test in enumerate(test_cases):
+        edges1 = test.get("edges1")
+        edges2 = test.get("edges2")
+        expected = test.get("expected")
+        result = sol.maxTargetNodes(edges1, edges2)
+        print(f"edges1 = {edges1} edges2 = {edges2}")
+        print(f"[largest path value: result = {result} and expected = {expected}]")
+        isEqual = True
+        if len(result) != len(expected):
+            isEqual = False
+        else:
+            for i in range(len(result)):
+                if result[i] != expected[i]:
+                    isEqual = False
+                    break
+        if isEqual: 
+            print(f"test {i}: \033[92mPass\033[0m")
+        else:
+            print(f"test {i}: \033[91mFail\033[0m")
 
 if __name__ == "__main__":
-    run_tests()
+    main()
+
--- maximize-target-nodes/solution_1.py
+++ maximize-target-nodes/solution_1.py
@@ -1,35 +1,49 @@
-def merge(left, right):
-    merge_a = []
-    i = j = 0
-    while i < len(left) and j < len(right):
-        if left[i] >= right[j]:
-            merge_a.append(left[i])
-            i+=1
-        else:
-            merge_a.append(right[j])
-            j+=1
-    if i < len(left):
-        merge_a.extend(left[i:])
-    if j < len(right):
-        merge_a.extend(right[j:])
-    return merge_a
+from typing import List
+from collections import deque
 
-def mergesort(array):
-    if len(array) <= 1:
-        return array
-    m = len(array) // 2
-    left = mergesort(array[:m])
-    right = mergesort(array[m:])
+class Solution:
+    def maxTargetNodes(self, edges1: List[List[int]],edges2: List[List[int]]) -> List[int]:
+        n = len(edges1) + 1
+        m = len(edges2) + 1
+        graph1 = [[] for _ in range(n)]
+        graph2 = [[] for _ in range(m)]
+        for u, v in edges1: 
+            graph1[u].append(v)
+            graph1[v].append(u)
+        for u, v in edges2: 
+            graph2[u].append(v)
+            graph2[v].append(u)
+        finalGraph = [-1]*n
+        maxin2 = 0
+        for i in range(m):
+            maxin2 = max(maxin2, (self.bfs(i, graph2, 1)-1))
+        for i in range(n):
+            count = self.bfs(i, graph1, 0)
+            finalGraph[i] = count + maxin2 
+        return finalGraph 
 
-    return merge(left, right)
+    def bfs(self, start: int, graph: List[List[int]], mul: int) -> int:
+        if len(graph) < 1:
+            return 1 
 
-def evenSubsetMax(nums):
-    numsSorted = mergesort(nums)
-    maxnum = 0
-    current = 0
-    for i in range(len(nums)):
-        current+=numsSorted[i]
-        if (i + 1) % 2 == 0:
-            maxnum = max(current, maxnum)
-    return maxnum
+        n = len(graph)
+        visited = [False]*n
+        dist = [-1]*n
+        q = deque([start])
+        visited[start] = True
+        dist[start] = 0
+        count = 1
+
+        while q:
+            u = q.pop()
+            for neighbor in graph[u]:
+                if not visited[neighbor]:
+                    q.append(neighbor)
+                    visited[neighbor] = True
+                    dist[neighbor] = dist[u] + 1
+                    if dist[neighbor] % 2 == mul:
+                        count += 1
+                    
+        return count 
+
 
