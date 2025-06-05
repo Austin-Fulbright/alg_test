ac6bfdd9c6b0b2a33f63b1ebf4feedca3918840d
Austin-Fulbright <austinfulbright@gmail.com>
2025-06-05 12:28:13 -0400

    added cat and mouse

--- cat-and-mouse/solution.py
+++ cat-and-mouse/solution.py
@@ -1,27 +1,69 @@
 from typing import List
 from collections import deque
+
 class Solution:
     def catMouseGame(self, graph: List[List[int]]) -> int: 
-        mouse = 1
-        cat = 2
-        hole = 0
-        def dfs(node, visited):
-            if node == 0:
-                return [0] 
-            best_path = None 
-            visited.add(node)
-            for neighbor in graph[node]:
-                if neighbor not in visited:
-                    subpath = dfs(neighbor, visited)
-                    if subpath is not None:
-                        candidate = [node] + subpath
-                    if best_path is None or len(candidate) < len(best_path):
-                        best_path = candidate 
-            visited.remove(node)
-            return best_path
+        HOLE = 0
+        MOUSE_START = 1
+        CAT_START = 2 
+        MOUSE_TURN = 0
+        CAT_TURN = 1
+        MOUSE_WIN = 1
+        CAT_WIN = 2
+        DRAW = 0
 
-        get_best_path = dfs(mouse, set()) 
+        n = len(graph)
+        result = [[[DRAW] * 2 for _ in range(n)] for _ in range(n)]
+        degree = [[[0] * 2 for _ in range(n)] for _ in range(n)]
+
+        for mouse_position in range(n):
+            for cat_position in range(1, n):
+                degree[mouse_position][cat_position][MOUSE_TURN] = len(graph[mouse_position])
+                degree[mouse_position][cat_position][CAT_TURN] = len(graph[cat_position]) - graph[cat_position].count(HOLE)
+
+        def get_prev_state(mouse_position, cat_position, turn):
+            prev_states = []
+            turn = 1 - turn
+            if turn == CAT_TURN:
+                for node in graph[cat_position]:
+                    if node != HOLE:
+                        prev_states.append((mouse_position, node, turn))
+            else:
+                for node in graph[mouse_position]:
+                    prev_states.append((node, cat_position, turn))
+            return prev_states
+
+        q = deque()
+
+        for i in range(n):
+            result[HOLE][i][MOUSE_TURN] = MOUSE_WIN
+            result[HOLE][i][CAT_TURN] = MOUSE_WIN
+            q.append((HOLE, i, MOUSE_TURN))
+            q.append((HOLE, i, CAT_TURN))
+
+            result[i][i][MOUSE_TURN] = CAT_WIN
+            result[i][i][CAT_TURN] = CAT_WIN
+            q.append((i, i, MOUSE_TURN))
+            q.append((i, i, CAT_TURN))
+
         
-        print(get_best_path)
+        while(q):
+            mouse_p, cat_p, turn = q.pop()
+            
+            cur_result = result[mouse_p][cat_p][turn] 
+            prev_states = get_prev_state(mouse_p, cat_p, turn) 
+            for mp, cp, pt in prev_states:
+                if result[mp][cp][pt] == DRAW:
+                    can_win = (cur_result == MOUSE_WIN and pt == MOUSE_TURN) or (cur_result == CAT_WIN and pt == CAT_TURN)
+                    if can_win:
+                        result[mp][cp][pt] = cur_result 
+                        q.append((mp, cp, pt))
+                    else:
+                        degree[mp][cp][pt] -= 1
+                        if degree[mp][cp][pt] == 0:
+                            result[mp][cp][pt] = cur_result 
+                            q.append((mp, cp, pt))
 
-        return 0 
+        return result[MOUSE_START][CAT_START][MOUSE_TURN]
+        
+
